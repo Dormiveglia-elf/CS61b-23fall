cbba977fba12f95505f04fad45956e63b88395d8
Zhenyu <120090196@link.cuhk.edu.cn>
2024-01-31 22:18:17 +0800

    Finished Lab 6

--- lab06/src/UnionFind.java
+++ lab06/src/UnionFind.java
@@ -9,34 +9,60 @@ public class UnionFind {
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        data = new int[N];
+        for (int i = 0; i < N; i++) {
+            data[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        int node = data[v];
+        while (node >= 0) {
+            node = data[node];
+        }
+        return -node;
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if (v < 0) {
+            return v;
+        } else {
+            return data[v];
+        }
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if (v < 0 || v >= data.length) {
+            throw new IllegalArgumentException("v should in the range [0, length-1], please try again");
+        }
+        int node = data[v];
+        int trace = v;
+        if (node < 0) {
+            return v;
+        } else {
+            while (data[node] >= 0) {
+                node = data[node];
+            }
+
+            while (data[trace] >= 0) {
+                int temp = data[trace];
+                data[trace] = node;
+                trace = temp;
+            }
+
+            return node;
+        }
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -45,7 +71,20 @@ public class UnionFind {
        root to V2's root. Union-ing a item with itself or items that are
        already connected should not change the structure. */
     public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
+        int root1 = find(v1);
+        int root2 = find(v2);
+
+        if (root1 == root2) {
+            return;
+        }
+
+        if (data[root1] < data[root2]) {
+            data[root1] += data[root2];
+            data[root2] = root1;
+        } else {
+            data[root2] += data[root1];
+            data[root1] = root2;
+        }
     }
 
     /**
