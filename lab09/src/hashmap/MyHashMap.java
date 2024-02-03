61c2b0182293f8e2050c4ba044efdc9e54b9193a
Zhenyu <120090196@link.cuhk.edu.cn>
2024-02-04 00:11:13 +0800

    Finished others, only put() method left

--- lab09/src/hashmap/MyHashMap.java
+++ lab09/src/hashmap/MyHashMap.java
@@ -1,6 +1,9 @@
 package hashmap;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
 
 /**
  *  A hash table-backed Map implementation.
@@ -9,7 +12,6 @@ import java.util.Collection;
  *  @author YOUR NAME HERE
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
-
     /**
      * Protected helper class to store key/value pairs
      * The protected qualifier allows subclass access
@@ -27,11 +29,22 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Instance Variables */
     private Collection<Node>[] buckets;
     // You should probably define some more!
+    private int N; // number of elements in the map
+    private int M; // number of buckets
+    private double loadFactor;
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this.loadFactor = 0.75;
+        this.M = 16;
+        buckets = new Collection[M];
+    }
 
-    public MyHashMap(int initialCapacity) { }
+    public MyHashMap(int initialCapacity) {
+        this.M = initialCapacity;
+        this.loadFactor = 0.75;
+        buckets = new Collection[M];
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialCapacity.
@@ -40,7 +53,11 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialCapacity initial size of backing array
      * @param loadFactor maximum load factor
      */
-    public MyHashMap(int initialCapacity, double loadFactor) { }
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        this.M = initialCapacity;
+        buckets = new Collection[M];
+        this.loadFactor = loadFactor;
+    }
 
     /**
      * Returns a data structure to be a hash table bucket
@@ -61,10 +78,72 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
+        return new LinkedList<>();
+    }
+
+
+    /** Associates the specified value with the specified key in this map.
+     *  If the map already contains the specified key, replaces the key's mapping
+     *  with the value specified. */
+    @Override
+    public void put(K key, V value) {
+        int bucketNum = Math.floorMod(key.hashCode(), M);
+    }
+
+    /** Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key. */
+    @Override
+    public V get(K key) {
+        for (int i = 0; i < M; i++) {
+            for (Node j: buckets[i]) {
+                if (j.key == key) {
+                    return j.value;
+                }
+            }
+        }
         return null;
     }
 
-    // TODO: Implement the methods of the Map61B Interface below
-    // Your code won't compile until you do so!
+    /** Returns whether this map contains a mapping for the specified key. */
+    @Override
+    public boolean containsKey(K key) {
+        for (int i = 0; i < M; i++) {
+            for (Node j: buckets[i]) {
+                if (j.key == key) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /** Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return this.N;
+    }
+
+    /** Removes every mapping from this map. */
+    @Override
+    public void clear() {
+        for (int i = 0; i < M; i++) {
+            buckets[i].clear();
+        }
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
 
 }
